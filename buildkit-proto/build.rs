b45875ebc7ca20da7cb53d80a316aca73c6dc930
kronoidz <alessandro.pecugi@outlook.it>
2025-02-28 16:27:27 +0100

    improve error reporting in proto build script

--- buildkit-proto/build.rs
+++ buildkit-proto/build.rs
@@ -1,11 +1,10 @@
 const DEFS: &[&str] = &["proto/github.com/moby/buildkit/frontend/gateway/pb/gateway.proto"];
 const PATHS: &[&str] = &["proto"];
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .compile_protos(DEFS, PATHS)?;
-
-    Ok(())
+        .compile_protos(DEFS, PATHS)
+        .unwrap_or_else(|e| panic!("{e}"));
 }
